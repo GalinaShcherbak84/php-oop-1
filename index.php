0e9d063b12861e5881a1b92a4915a33192039da7
GalinaSherbak84 <galinasherbak@gmail.com>
2021-05-31 18:02:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+class Movie{
+    //Attributi/proprieta/variabili
+    public $title;
+    public $year;
+    public $description;
+    //Costruttore
+    function __construct($title, $year, $description){
+       $this->title = $title;
+       $this->year = $year;
+       $this->description = $description; 
+    }
+}
+
+//instanza movie 1
+$movie1 = new Movie('Movie','1999','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio sapien,
+tincidunt ac bibendum et, tempor sit amet arcu. Praesent ut sem at purus tempus placerat sit amet id diam.
+Nullam id malesuada lectus.');
+var_dump($movie1);
+
+//instanza movie 2
+$movie2 = new Movie('Movie-Movie','2020',' Mauris nec urna mattis, hendrerit lectus eget,
+dictum mi. Praesent non dignissim tellus, non ullamcorper felis. Phasellus iaculis bibendum bibendum.
+Duis elementum elit non pulvinar pretium.');
+var_dump($movie2);
+
